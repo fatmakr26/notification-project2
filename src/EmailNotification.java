28bc5b22c2989e0569e9b7c0cda746113d3a9e3c
fatmakr26 <fatmakr2650@gmail.com>
2026-05-06 22:53:52 +0300

    Create EmailNotification.java

---
+++ src/EmailNotification.java
@@ -0,0 +1,8 @@
+package com.example.notification;
+
+public class EmailNotification implements Notification {
+    @Override
+    public void send(String message) {
+        System.out.println("Email gönderildi: " + message);
+    }
+}
