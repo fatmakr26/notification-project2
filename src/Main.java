3480f62569f144696297cf0817458298ba235458
fatmakr26 <fatmakr2650@gmail.com>
2026-05-08 16:17:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ package com.example.notification;
 public class Main {
     public static void main(String[] args) {
 
-        // ===== Facade =====
         NotificationFacade facade = new NotificationFacade();
         facade.send("email", "Hello Email!");
         facade.send("sms", "Hello SMS!");
 
-        // ===== Adapter =====
         Notification adapter = new EmailAdapter(new ThirdPartyEmailService());
         adapter.send("Adapter email message");
     }
