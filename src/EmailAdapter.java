d1a8b40bd79d1a97744058f64cbadf22acbf1719
fatmakr26 <fatmakr2650@gmail.com>
2026-05-08 16:03:22 +0300

    Create EmailAdapter.java
    feat: add EmailAdapter to integrate ThirdPartyEmailService

---
+++ src/EmailAdapter.java
@@ -0,0 +1,14 @@
+package com.example.notification;
+
+public class EmailAdapter implements Notification {
+    private ThirdPartyEmailService service;
+
+    public EmailAdapter(ThirdPartyEmailService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void send(String message) {
+        service.sendEmail(message);
+    }
+}
