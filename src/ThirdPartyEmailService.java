1f3e6736aa2d2cd297972eb009721ee1715ab317
fatmakr26 <fatmakr2650@gmail.com>
2026-05-12 22:07:20 +0300

    feat: add legacy third party email service simulation

--- src/ThirdPartyEmailService.java
+++ src/ThirdPartyEmailService.java
@@ -1,6 +1,5 @@
 package com.example.notification;
 
-// Simulated legacy/third-party service
 public class ThirdPartyEmailService {
     public void sendEmail(String content) {
         System.out.println("[ThirdPartyEmailService] " + content);
